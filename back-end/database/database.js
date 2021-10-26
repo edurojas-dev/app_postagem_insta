9a817fea17754907f4ba21d256c13682be131b04
Eduardo Rojas <89988885+edurojas-nagro@users.noreply.github.com>
2021-10-26 16:49:07 -0300

    Update database.js

--- back-end/database/database.js
+++ back-end/database/database.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize")
 const connection = new Sequelize(
     "postagem_insta",   // name database
     "root",             //user root
-    "dudu2020",         //password
+    "xxxxxxx",         //password
     {host: "localhost", dialect: "mysql"}
 )
 
